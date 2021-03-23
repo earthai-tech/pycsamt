12df889b87168191fa2ef87a810537e1acd21024
Kouadio K.Laurent <etanoyau@gmail.com>
2021-03-23 20:13:18 +0800

    Add add ama filter and the way to ouptput edi with ama filter

--- scripts/cs_write_avg_to_edi.py
+++ scripts/cs_write_avg_to_edi.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-    .Script to write edifiles from Zonge Engineering AVG format . Script accept 3 filters "
-    "TMA" : Triming Moving Average , "AMA": Adaptative Moving Average of Torres abd verding 
-    "FLMA": Fixed Length dipole moving average . Actuallyfilters "TMA & FLMA" work, 
-    AMA filter will be add soon. Once applied it , it will generate Rho corrected into 
-    your output edifiles. If filter is None , just write edifile , no filter will be applied. 
-    If  zonge astatic file is given , program will take account to write both resistivites . 
-    the non corrected apparent resistivities RHO and the static shift corrected FRHO. 
-   
+    .Script to write edifiles from Zonge Engineering AVG format .
+    Script accept 3 filters , TMA" : Triming Moving Average ,
+     "AMA": Adaptative Moving Average of Torres-Verdin
+    "FLMA": Fixed Length dipole moving average . If filter is applied 
+    Rho corrected shoud be in output edifiles as FRHO. If filter
+    is None , just write edifile. If  zonge astatic file is given , it assumes 
+    a filter is already applied, then will read `avg` file and set filter 
+    resistivities values as FRHO, means , `edi`outputfile 
+    will contain static shift corrected FRHO and Uncorrected resistivities. 
+
 Created on Sat Jan 16 19:55:58 2021
 
 @author: @Daniel03
@@ -30,9 +32,14 @@ avgfile = 'K1.AVG'
 station_profile_file = 'K1.stn'
 
 #---Appply filter . 
-add_filter = None              # can be "tma" or "lma" filters , if filter is not None
+add_filter = 'ama'              # can be "tma", 'flma' or "ama" filters , if filter is not None
                                 # will compute FRHO
-                               
+#------------------------------------------------------------------------------
+# when applied filter , can set the number of points for FLMA or TMA filter and number of skin depth 
+# for AMA filters. if add filter is None , numberof points and skin depth will turn off. 
+number_of_points =7.            # default is 7. can be change to any value you want 
+number_of_skin_depth =7.        # default is 3. 
+#------------------------------------------------------------------------------                       
 # add reference frequency 
 reference_frequency = None      #8192  # if reference frequency is None AND add filter is not None , 
                                 # reference frequency  will be compute automatically  
@@ -44,5 +51,7 @@ avg_obj.avg_to_edifile(data_fn= os.path.join(path_to_avgfile, avgfile) ,
                        profile_fn = os.path.join(path_to_avgfile, station_profile_file), 
                        savepath =save_edipath, 
                        reference_frequency= reference_frequency, 
-                       apply_filter=add_filter ) 
+                       apply_filter=add_filter, 
+                       number_of_points=number_of_points, 
+                       number_of_skin_depth=number_of_skin_depth) 
 

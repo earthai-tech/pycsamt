ec8dcf3340973a55cc43688ef82833d34ab00e40
Kouadio K.Laurent <etanoyau@gmail.com>
2021-03-23 20:12:30 +0800

    Add add ama filter and rewrite the correction script

--- scripts/write_corrected_edi.py
+++ scripts/write_corrected_edi.py
@@ -2,12 +2,12 @@
 """
    .Script to write corrected edi by applying filter. Data can be Electromagnetic 
    Array Profiling(EMAP) or Magnetotelluric data (MT). Available filters are
-   `tma`, `flma`, `ss` and `dist`.'Trimming moving-average' `tma` or
-   Fixed-length dipole moving average `flma` are mainly used to corrected
-   EMAP data , however can use to force processing MT data. static shift removal
-  `ss` and distorsion removal `dist` are usually use to process MT data.
-   Adaptative moving average `ama` is not available yet.
-   
+   `tma`, `flma`, `ss` and `dist`.'Trimming moving-average' `tma` or Adaptative
+   moving average `ama` or Fixed-length dipole moving average `flma` are mainly 
+   used to corrected EMAP data , however can use to force processing MT data.
+   static shift removal `ss` and distorsion removal `dist` are usually use
+   to process MT data.
+
 Created on Fri Mar 19 19:15:46 2021
 
 @author: @Daniel03
@@ -19,14 +19,17 @@ from csamtpy.ff.processing.corr import shifting
 # from csamtpy.ff.processing.corr import shifting 
 
 # profile edipath : full path to edifiles or single edifile 
-edipath =os.path.join(os.environ['pyCSAMT'], 'data','_outputEDIFiltered_FLMA') #'edi')#, 'new_csa000.edi' )
+edipath =os.path.join(os.environ['pyCSAMT'], 'data','_outputEDIFiltered_AMA') #'edi')#, 'new_csa000.edi' )
 
 # Applied filter 
-FILTER = 'flma'                        # availables filters [`tma`, `flma`, `ss`, `dist`]
+FILTER = 'ama'                        # availables filters [`tma`, `flma`,`ama`, `ss`, `dist`]
                                         # default is `tma`
                                         
 # number of points : to computed the window width  
 number_of_filter_points = 7.            # default is 7. set to 01. when use si ngle edifiles 
+# number of skin depth : specially provided to compute rho ith AMA filter 
+number_of_skin_depth =7.                # default is 3. can be 1 to 10 skin depths 
+
 # reference frequency 
 reference_frequency = 8192.             # frequency at clean data , usefull when data is EMAP data 
                                         # not use for MT data 
@@ -60,6 +63,7 @@ distortion_err_tensor = None
 corr_obj= shifting().write_corrected_edi(data_fn = edipath, 
                              number_of_points =number_of_filter_points,
                              reference_frequency=reference_frequency,
+                             number_of_skin_depth=number_of_skin_depth, 
                              dipole_length =dipoleLength, 
                              FILTER=FILTER, 
                              edi_newname = new_edifilename, 

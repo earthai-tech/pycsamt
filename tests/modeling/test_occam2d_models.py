1165809b2851ce73498dad8ab85d52e245ca77e8
Kouadio K.Laurent <etanoyau@gmail.com>
2021-04-01 20:03:03 +0800

    test write occam2d to write inputfiles

--- tests/modeling/test_occam2d_models.py
+++ tests/modeling/test_occam2d_models.py
@@ -2,8 +2,8 @@
 # ! /usr/bin/env python
 """
 Description:
-    Test create input files required to read  occam2d inversion model files, which include:
-    ('Occam2DMesh',    'Occam2DModel',    'Startup', 'IterXX.iter')
+    Test create input files required to read  occam2d inversion model files, which includes:
+    ('Occam2DMesh',    'Occam2DModel',    'Startup', 'OccamDataFile.dat')
     These input files are created from standard edi data file set.
 
 References:
@@ -12,100 +12,110 @@ References:
 CreationDate:   25/03/2021
 Developer:      kkouao@zju.edu.cn
 
-inspired from script of Zhang Fei of Geosciences Australia: fei.zhang@ga.gov.au
     
 """
-from . import reset_matplotlib
-from . import diff_files
+from tests.modeling.__init__ import reset_matplotlib, csamtpylog, diff_files
 
-import os
-from unittest import TestCase
+import os 
 
-# import csamtpy.modeling.occam2d as occam2d
-from viewer.plot import Plot2d
+import  unittest 
+
+from csamtpy.modeling.occam2d import occam2d_write
+
 
 from tests import EDI_DATA_DIR, OC2D_DIR, make_temp_dir,TEST_TEMP_DIR 
 
+class TestWriteOccam2DInputfiles(unittest.TestCase):
+    """
+    Test to generate Occam2D inputfiles such as 
+    `Startup` , 'Occam2DMesh', 'Occam2DModel', 'OccamDataFile.dat' files.
+    pyCSAMT try to intall `MTpy` packages if not exists'. If an errors occurs, 
+    may try to install `MTpy` mannually. 
+    
+    """
 
-
-class TestOccam2D(TestCase):
-    @classmethod
+    _expected_edi_dir = EDI_DATA_DIR 
+    _expected_save_dir = TEST_TEMP_DIR 
+    geo_electric_strike = 34.
+    
+    @classmethod 
     def setUpClass(cls):
+        """
+        Reset building matplotlib plot and generate tempdir inputfiles 
+        
+        """
         reset_matplotlib()
         cls._temp_dir = make_temp_dir(cls.__name__)
 
-    def setUp(self):
-
-        # set the dir to the output from the previously correct run
-        self._expected_output_dir = os.path.join(OC2D_DIR, 'Occam2d')
-
-        if not os.path.isdir(self._expected_output_dir):
-            self._expected_output_dir = None
-
-        # directory to save readed outputfiles t files
-        self._output_dir = make_temp_dir('Occam2d', self._temp_dir)
-
-    def test_fun(self):
-        """
-
-        :return:
-        """
+    def setUp(self): 
         
-        outdir = self._main_func(edipath=EDI_DATA_DIR)
+        if not os.path.isdir(self._expected_edi_dir):
+            print('--> outdir not exist , set to None !')
+            self._expected_edi_dir =None 
+        
 
-        for afile in ('Occam2DMesh', 'Occam2DModel', 'Occam2DStartup'):
-            output_data_file = os.path.join(outdir, afile)
-            self.assertTrue(os.path.isfile(output_data_file), "output data file not found")
+    def test_occam2d_buildingInputfiles(self): 
+        """
+        Test occam2d inputbuilding files . 
+        First check if code succeffully run and secundly check whether the 
+        expected files much the number of files generated as  well as 
+        the ouputfiles names .
+        
+        """
+        for file  in [self._expected_edi_dir , self._expected_edi_dir]: 
+            self.assertIsInstance(file, str,
+                                  'Expected outputfiles must be PathLike str !')
+            
+        try : 
+            stations_list = [os.path.join(self._expected_edi_dir, file) 
+             for file in os.listdir(self._expected_edi_dir) if 
+                             self._expected_edi_dir.endswith('.edi')]
+        except : 
+            csamtpylog.get_csamtpy_logger().error( 
+                'something wrong when call edifiles in %s' % self._expected_edi_dir )
+        else : 
+            print('...> {0} stations successfully  read'.format(len (stations_list)))
+            csamtpylog.get_csamtpy_logger().info( 
+                'Now write occam2d inputfuiles in %s' % self._expected_edi_dir )
+            occam2d_write.buildingInputfiles(edi_fn= self._expected_edi_dir,
+                                   savepath =os.path.join(self._expected_save_dir,
+                                                          self.__class__.__name__), 
+                                   geoelectric_strike= self.geo_electric_strike)
+            
+        # now assert expected files and output files 
+        # check whether the tem dir files if create
+        self.assertTrue(os.path.exists(os.path.join(TEST_TEMP_DIR , self.__class__.__name__ )),
+                                       
+                                       'Temp dir path does not exists')
+        
+        # now control existing files 
+        out_tem_dir = os.path.join(TEST_TEMP_DIR ,self.__class__.__name__ )
 
-            expected_data_file = os.path.join(self._expected_output_dir, afile)
-
+        output_data_file =  [os.path.join(out_tem_dir, file) for file in os.listdir(out_tem_dir)]
+    
+        output_data_file = sorted( output_data_file) 
+        
+        self.assertEqual(len(output_data_file), 4,
+                         'Number of Occam2D inputfiles  built is %s ,'
+                         ' far from expected 4 files.'%len(output_data_file))
+        
+        # now check the name of generate files and compared to expected files 
+        for ii, afile in enumerate( sorted(('Occam2DMesh', 'Occam2DModel',
+                                            'OccamDataFile.dat', 'Startup'))):
+            expected_data_file = os.path.join(out_tem_dir, afile) 
+            
             self.assertTrue(os.path.isfile(expected_data_file),
-                            "Ref output data file does not exist, nothing to compare with"
-                            )
-
-            print(("Comparing", output_data_file, "and", expected_data_file))
-
-            is_identical, msg = diff_files(output_data_file, 
+                                "Ref output data file does not exist, nothing to compare with"
+                                )
+        
+            print(("Comparing", output_data_file[ii], "and", expected_data_file))
+            
+            is_identical, msg = diff_files(output_data_file[ii], 
                                            expected_data_file, ignores=['Date/Time:'])
             print(msg)
             self.assertTrue(is_identical, "The output file is not the same with the baseline file.")
-
-    def test_plot_model_and_responses(self):
-        """
-            test function
-            :return: T/F
-            """
-        # imaging depth : Maximum depth investigation 
-        doi = '1km'                 #  can be float like 1000 = 1km 
-        
-        #plot style 
-        plotStyle ="pcolormesh"            # if None Default is 'imshow', can be 
-                                    #["pcolormesh"]
-        
-        savefigure = os.path. join(TEST_TEMP_DIR, 'k1.png')
-        #-----OCCAM 2D output data files ---------
-        # path to occam Data file
-        path_to_occam_data='OccamDataFile.dat'
-        
-        # path to occam Mesh file 
-        path_to_occam_mesh = 'Occam2DMesh'
-        
-        # path to occam Model file 
-        path_to_occam_model = 'Occam2DModel'
-        # path to Occam Iteration file 
-        path_to_occam_iter='ITER10.iter'
-        
-        # show a report 
-        see_report =False      # generate groundwater report           
-        
-        figsize =[9,9]
-        # call plot obj 
-        plot2d_obj = Plot2d(fig_size =figsize)#,fig_dpi = 600. )
-        plot2d_obj.plot_occam2dModel(mesh_fn=os.path.join(OC2D_DIR, path_to_occam_mesh), 
-                            iter_fn = os.path.join(OC2D_DIR, path_to_occam_iter), 
-                            model_fn =os.path.join(OC2D_DIR, path_to_occam_model ), 
-                            data_fn =os.path.join(OC2D_DIR, path_to_occam_data), 
-                            doi= doi, 
-                            savefig =savefigure, 
-                            plot_style =plotStyle, 
-                            show_report = see_report )
+            
+if __name__=='__main__':    
+    unittest.main()
+    
+   

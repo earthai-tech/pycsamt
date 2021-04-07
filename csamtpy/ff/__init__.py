67764d5899ca894689aaa9e24f63eec44991d4f8
Kouadio K.Laurent <etanoyau@gmail.com>
2021-04-07 21:49:20 +0800

    csamtpy folder removed

--- csamtpy/ff/__init__.py
+++
@@ -1,8 +0,0 @@
-from .core import *
-# from .avg import * 
-# from .cs import * 
-# from .edi import * 
-# from .j import * 
-# from .z import * 
-
-
--- csamtpy/ff/gui/__init__.py
+++
@@ -1,5 +0,0 @@
-from .gui_avgapp import *
-from .gui_frames import *
-from .maingui import *
-
-__all__=[ '*', '*', '*']

f2817cd847eb5c46d40b057ade8d2430ea4f0879
Kouadio K.Laurent <etanoyau@gmail.com>
2021-04-19 20:31:21 +0800

    slight edited

--- scripts/plot_survey_lines.py
+++ scripts/plot_survey_lines.py
@@ -4,8 +4,8 @@
     Engineering file. If such file is not at available , use 
     module  `rewrite_station_profile` from Profile class  to write *.stn file
     so to call it directly. 
-        ::>>> from pycsamt.pyCS.core.cs import Profile 
-        :::Profile.rewrite_station_profile(easting , northing, **kws) :: 
+        >>> from pycsamt.ff.core.cs import Profile 
+        :Profile().rewrite_station_profile(easting , northing, **kws): 
     User can provide also provide  `X`, `Y` coordinates
     for each survey  lines  on a list of eastings and northings.
     Can custmize plot using differents matplotlib properties 
@@ -43,7 +43,6 @@ scale ='km'                     # can be `m` or `km` . Default is `m`
 
 #set to False if you dont want to see stations labels 
 show_station_labels = True                  # default is TRue  
-
 
  
 figsize  =[5,3]                 # figure size 
